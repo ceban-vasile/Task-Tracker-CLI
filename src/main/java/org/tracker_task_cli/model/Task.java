43b9bd1756a66cf64eade61c082260b9c553bef6
Vasile <vasile.ceban1@isa.utm.md>
2025-04-17 09:50:11 +0300

    implement in FileHAndler class read, delete and find by id task method and change the Task class

--- src/main/java/org/tracker_task_cli/model/Task.java
+++ src/main/java/org/tracker_task_cli/model/Task.java
@@ -3,72 +3,101 @@ package org.tracker_task_cli.model;
 import java.util.Date;
 
 public class Task {
-    private final int id;
-    private final String description;
-    private final Status status;
-    private final Date createdAt, updatedAt;
+    private int id;
+    private String description;
+    private Status status;
+    private Date createdAt;
+    private Date updatedAt;
 
-    private Task(Builder builder) {
-        this.id = builder.id;
-        this.description = builder.description;
-        this.status = builder.status;
-        this.createdAt = builder.createdAt;
-        this.updatedAt = builder.updatedAt;
+    public Task() {
     }
-     public static class Builder{
-        private int id;
-        private String description;
-        private Status status;
-        private Date createdAt;
-        private Date updatedAt;
 
-         public Builder setId(int id) {
-             this.id = id;
-             return this;
-         }
-
-         public Builder setDescription(String description) {
-             this.description = description;
-             return this;
-         }
-
-         public Builder setStatus(Status status) {
-             this.status = status;
-             return this;
-         }
-
-         public Builder setCreatedAt(Date createdAt) {
-             this.createdAt = createdAt;
-             return this;
-         }
-
-         public Builder setUpdatedAt(Date updatedAt) {
-             this.updatedAt = updatedAt;
-             return this;
-         }
-
-         public Task build(){
-             return new Task(this);
-         }
-     }
+    private Task(int id, String description, Status status, Date createdAt, Date updatedAt) {
+        this.id = id;
+        this.description = description;
+        this.status = status;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
 
     public int getId() {
         return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getDescription() {
         return description;
     }
 
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
     public Status getStatus() {
         return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
     }
 
     public Date getCreatedAt() {
         return createdAt;
     }
 
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
     public Date getUpdatedAt() {
         return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public static class Builder {
+        private int id;
+        private String description;
+        private Status status;
+        private Date createdAt;
+        private Date updatedAt;
+
+        public Builder setId(int id) {
+            this.id = id;
+            return this;
+        }
+
+        public Builder setDescription(String description) {
+            this.description = description;
+            return this;
+        }
+
+        public Builder setStatus(Status status) {
+            this.status = status;
+            return this;
+        }
+
+        public Builder setCreatedAt(Date createdAt) {
+            this.createdAt = createdAt;
+            return this;
+        }
+
+        public Builder setUpdatedAt(Date updatedAt) {
+            this.updatedAt = updatedAt;
+            return this;
+        }
+
+        public Task build() {
+            return new Task(id, description, status, createdAt, updatedAt);
+        }
+    }
+
+    public String toString() {
+        return "Task:\n id=" + id + ",\n description=" + description + ",\n status=" + status + ",\n createdAt=" + createdAt + ",\n updatedAt=" + updatedAt + "\n";
     }
 }
